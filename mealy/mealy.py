a4f9a317f53b0113962515efcbdbd027b7a69cca
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-10-11 00:33:51 -0500

    regular expressions and address validations have been corrected.

--- mealy/mealy.py
+++ mealy/mealy.py
@@ -69,8 +69,10 @@ class State:
 
     def walk(self, steps: str) -> Iterator[MealyResult]:
         state = self
-        words = re.split(" ",steps)
- 
+        words = re.split(r"[- #]",steps)
+        
+        words = list(filter(None, words))
+        print(words)
         for word in words:
 
             print(word)
@@ -140,51 +142,51 @@ def test_mealy():
     q33 = State("q33")
     q34 = State("q34")
     q35 = State("q35")
-    q36 = State("q36")
-    q37 = State("q37")
     
  
-    q0.set_paths([Path("tv", q1, "0",r'\b(AU|AV|AC|AK|BL|CL|KR|CT|CQ|CV|CC|DG|PJ|PS|PT|TV|VT|VI)\b'), Path("br2", q30, "0",r'\b(BR|CD|SM)\b'),Path("vda", q25, "0",r'\bVDA\b'),Path("km", q21, "0",r'\bKM\b')])
+    q0.set_paths([Path("tv", q1, "0",r'\b(Autopista|Avenida|Avenida Calle|Avenida Carrera|Bulevar|Calle|Carrera|Carretera|Circular|Circunvalar|Cuentas Corridas|Diagonal|Pasaje|Paseo|Peatonal|Transversal|Variante|Via|Troncal)\b'), Path("br2", q19, "0",r'\b(Barrio|Ciudadela|Supermanzana)\b'),Path("vda", q27, "0",r'\bVereda\b'),Path("km", q31, "0",r'\bKilometro\b')])
     #Urbana
-    q1.set_paths([Path("nv", q2, "0",r'\b[1-9][0-9]{0,2}\b')])
-    q2.set_paths([Path("lv", q3, "0",r'^(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
-    q3.set_paths([Path("bis", q4, "0",r'^BIS$')])
-    q4.set_paths([Path("lp", q5, "0",r'^(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
-    q5.set_paths([Path("c", q6, "0",r'^(NORTE|SUR|ESTE|OESTE)$')])
-    q6.set_paths([Path("nv", q7, "0",r'\b[1-9][0-9]{0,2}\b')])
-    q7.set_paths([Path("lv", q8, "0",r'^(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
-    q8.set_paths([Path("bis", q9, "0",r'^BIS$')])
-    q9.set_paths([Path("ls", q10, "0",r'^(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
-    q10.set_paths([Path("np", q11, "1",r'\b[1-9][0-9]{0,2}\b')])
-    q11.set_paths([Path("c", q20, "1",r'^(NORTE|SUR|ESTE|OESTE)$')])
-    q20.set_paths([Path("br", q12, "0",r'^(BR|CD|SM)$')])
-    q12.set_paths([Path("nb", q13, "1",r'^\w+$')])
-    q13.set_paths([Path("u", q14, "0",r'^(BQ|CU|CO|ET|UR|SC|TO|ZN)$')])
-    q14.set_paths([Path("nu", q15, "1",r'^\w+$')])
-    q15.set_paths([Path("ma", q16, "0",r'^(MZ|IN|SC|ET|ED|MD|TO)$')])
-    q16.set_paths([Path("nma", q17, "1",r'^\w+$')])
-    q17.set_paths([Path("tp", q18, "0",r'^(AL|AP|BG|CD|CN|DP|DS|GA|GS|LC|LM|LT|MN|OF|PA|PN|PL|PD|SS|SO|ST|TZ|UN|UL)$')])
-    q18.set_paths([Path("ip", q19, "1",r'^\w+$')])
-    #Km y via //rural
-    q21.set_paths([Path("nkm", q22, "0",r'^[1-9][0-9]{0,1}$')])
-    q22.set_paths([Path("v", q23, "0",r'^V$')])
-    q23.set_paths([Path("n1", q24, "1",r'^[A-Z]+$')])
-    q24.set_paths([Path("n2", q29, "1",r'^[A-Z]+$')])
-    #Barrio // urbana
-    q30.set_paths([Path("nb2", q31, "0",r'^\w+$')])
-    q31.set_paths([Path("u2", q32, "0",r'^(BQ|CU|CO|ET|UR|SC|TO|ZN)$')])
-    q32.set_paths([Path("nu2", q33, "0",r'^\w+$')])
-    q33.set_paths([Path("ma2", q34, "0",r'^(MZ|IN|SC|ET|ED|MD|TO)$')])
-    q34.set_paths([Path("nma2", q35, "0",r'^\w+$')])
-    q35.set_paths([Path("tp", q36, "0",r'^(AL,AP,BG,CD,CN,DP,DS,GA,GS,LC,LM,LT,MN,OF,PA,PN,PL,PD,SS,SO,ST,TZ,UN,UL)$')])
-    q36.set_paths([Path("ip", q37, "1",r'^\w+$')])
-    #Vereda // rural
-    q25.set_paths([Path("nvda", q26, "1",r'^\w+$')])
-    q26.set_paths([Path("sc", q27, "0",r'^SC$')])
-    q27.set_paths([Path("nsc", q28, "1",r'^\w+$')])
+    q1.set_paths([Path("nv", q2, "0",r'^[1-9][0-9]{0,2}(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
+    q2.set_paths([Path("bis", q3, "0",r'^BIS$'),Path("c", q5, "0",r'^(NORTE|SUR|ESTE|OESTE)$'),Path("nv", q6, "0",r'^[1-9][0-9]{0,2}(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
+    q3.set_paths([Path("lp", q4, "0",r'^(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
+    q4.set_paths([Path("c", q5, "0",r'^(NORTE|SUR|ESTE|OESTE)$')])
+    q5.set_paths([Path("nv", q6, "0",r'^[1-9][0-9]{0,2}(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
+    q6.set_paths([Path("bis", q7, "0",r'^BIS$'),Path("np", q9, "1",r'\b[1-9][0-9]{0,2}\b')])
+    q7.set_paths([Path("lp", q8, "0",r'^(([A-Z][1-9][A-Z])|([A-Z]{2})|([A-Z]{0,1})|)$')])
+    q8.set_paths([Path("np", q9, "1",r'\b[1-9][0-9]{0,2}\b')])
+    q9.set_paths([Path("c", q10, "1",r'^(NORTE|SUR|ESTE|OESTE)$'),Path("br", q11, "0",r'^(Barrio|Ciudadela|Supermanzana)$'),Path("u", q13, "0",r'^(Bloque|Celula|conjunto Residencial|Etapa|Urbanizacion|Sector|Torre|Zona)$'),Path("ma", q15, "0",r'^(Manzana|Interior|Sector|Etapa|Edificio|Modulo|Torre)$'),Path("tp", q17, "0",r'^(Altillo|Apartamento|Bodega|Casa|Consultorio|Deposito|Garaje|Local|Lote|Mezzanine|Oficina|Parqueadero|Pent-House|Planta|Predio|Semisotano|Sotano|Suite|Terraza|Unidad)$')])
+    q10.set_paths([Path("br", q11, "0",r'^(Barrio|Ciudadela|Supermanzana)$'),Path("u", q13, "0",r'^(Bloque|Celula|conjunto Residencial|Etapa|Urbanizacion|Sector|Torre|Zona)$'),Path("ma", q15, "0",r'^(Manzana|Interior|Sector|Etapa|Edificio|Modulo|Torre)$'),Path("tp", q17, "0",r'^(Altillo|Apartamento|Bodega|Casa|Consultorio|Deposito|Garaje|Local|Lote|Mezzanine|Oficina|Parqueadero|Pent-House|Planta|Predio|Semisotano|Sotano|Suite|Terraza|Unidad)$')])
+    q11.set_paths([Path("nb", q12, "1",r'^\w+$')])
+    q12.set_paths([Path("u", q13, "0",r'^(Bloque|Celula|conjunto Residencial|Etapa|Urbanizacion|Sector|Torre|Zona)$'),Path("ma", q15, "0",r'^(Manzana|Interior|Sector|Etapa|Edificio|Modulo|Torre)$'),Path("tp", q17, "0",r'^(Altillo|Apartamento|Bodega|Casa|Consultorio|Deposito|Garaje|Local|Lote|Mezzanine|Oficina|Parqueadero|Pent-House|Planta|Predio|Semisotano|Sotano|Suite|Terraza|Unidad)$')])
+    q13.set_paths([Path("nu", q14, "1",r'^\w+$')])
+    q14.set_paths([Path("ma", q15, "0",r'^(Manzana|Interior|Sector|Etapa|Edificio|Modulo|Torre)$'),Path("tp", q17, "0",r'^(Altillo|Apartamento|Bodega|Casa|Consultorio|Deposito|Garaje|Local|Lote|Mezzanine|Oficina|Parqueadero|Pent-House|Planta|Predio|Semisotano|Sotano|Suite|Terraza|Unidad)$')])
+    q15.set_paths([Path("nma", q16, "1",r'^\w+$')])
+    q16.set_paths([Path("tp", q17, "0",r'^(Altillo|Apartamento|Bodega|Casa|Consultorio|Deposito|Garaje|Local|Lote|Mezzanine|Oficina|Parqueadero|Pent-House|Planta|Predio|Semisotano|Sotano|Suite|Terraza|Unidad)$')])
+    q17.set_paths([Path("ip", q18, "1",r'^\w+$')])
     
-   
-    q0.walk("TV 12 A BIS A NORTE 12 A BIS AB 12 NORTE BR SAN_LUCAS BQ 5 MZ 1 AP 201")
+    #Barrio // urbana
+    q19.set_paths([Path("nb", q20, "0",r'^\w+$')])
+    q20.set_paths([Path("u", q21, "0",r'^(Bloque|Celula|conjunto Residencial|Etapa|Urbanizacion|Sector|Torre|Zona)$')])
+    q21.set_paths([Path("nu", q22, "0",r'^\w+$')])
+    q22.set_paths([Path("ma", q23, "0",r'^(Manzana|Interior|Sector|Etapa|Edificio|Modulo|Torre)$')])
+    q23.set_paths([Path("nma", q24, "1",r'^\w+$')])
+    q24.set_paths([Path("tp", q25, "0",r'^(Altillo|Apartamento|Bodega|Casa|Consultorio|Deposito|Garaje|Local|Lote|Mezzanine|Oficina|Parqueadero|Pent-House|Planta|Predio|Semisotano|Sotano|Suite|Terraza|Unidad)$')])
+    q25.set_paths([Path("ip", q26, "1",r'^\w+$')])
+    
+    #Vereda // rural
+    q27.set_paths([Path("nvda", q28, "1",r'^\w+$')])
+    q28.set_paths([Path("sc", q29, "0",r'^Sector$')])
+    q29.set_paths([Path("nsc", q30, "1",r'^\w+$')])
+    #Km y via //rural
+    q31.set_paths([Path("nmk", q32, "0",r'^[1-9][0-9]{0,2}$')])
+    q32.set_paths([Path("v", q33, "0",r'^Via$')])
+    q33.set_paths([Path("n1", q34, "1",r'^\w+$')])
+    q34.set_paths([Path("n2", q35, "1",r'^\w+$')])
+    
+    # q0.walk("Carrera 7B #1-20 Apartamento 201")
+    # q0.walk("Vereda San_Juan Sector La_uida")
+    # q0.walk("Barrio San_Juan Etapa 3 Manzana 4 Apartamento 501")
+    q0.walk("Kilometro 32 Via Duitama-Paipa")
     
 
 if __name__ == "__main__":
