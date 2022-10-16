c7682e6c9587117eefdf67e0867a648342e6b940
Astrid Cely Cárdenas <61441594+AstridCely@users.noreply.github.com>
2022-10-16 14:46:08 -0500

    example of the address_validator_dian function

--- examples/examples.py
+++ examples/examples.py
@@ -1,12 +1,21 @@
-from addressValidator import address_validator
+from addressValidator import address_validator,address_validator_dian
 
 def test_address_validator() -> str:
 
     address = "Calle 3BC #10-2 Barrio San Juan Apartamento 201"
     if address_validator(address):
-       return "direccion valida"
+       return address+" direccion valida"
     else:
-       return "direccion invalida"
+       return address+" direccion invalida"
+
+def test_address_validator_dian() -> str:
+
+    address = "AV 33 B2C BIS 10 21 BR SOCORRO AP 2"
+    if address_validator_dian(address):
+       return address+" direccion valida"
+    else:
+       return address+" direccion invalida"
 
 if __name__ == "__main__":
     print(test_address_validator())
+    print(test_address_validator_dian())
