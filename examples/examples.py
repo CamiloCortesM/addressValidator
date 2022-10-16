dbce4ccc5dee26d7c5dc82493227f98387ab77ba
Astrid Cely Cárdenas <61441594+AstridCely@users.noreply.github.com>
2022-10-16 14:33:45 -0500

    firts example in address_validator function

--- examples/examples.py
+++ examples/examples.py
@@ -1,2 +1,12 @@
 from addressValidator import address_validator
 
+def test_address_validator() -> str:
+
+    address = "Calle 3BC #10-2 Barrio San Juan Apartamento 201"
+    if address_validator(address):
+       return "direccion valida"
+    else:
+       return "direccion invalida"
+
+if __name__ == "__main__":
+    print(test_address_validator())
